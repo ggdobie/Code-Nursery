d17ea095a578dcbe308d34bfdf178d7b694340c5
Graham <graham@silcrow.co>
2022-07-27 13:07:04 +0000

    feat: complete linear scroll behaviour

--- logo-grid/scroll.js
+++ logo-grid/scroll.js
@@ -10,6 +10,10 @@ function map(value, low1, high1, low2, high2) {
   return low2 + ((high2 - low2) * (value - low1)) / (high1 - low1);
 }
 
+function easeInOutCubic(t) {
+  return t < 0.5 ? 4 * t * t * t : (t - 1) * (2 * t - 2) * (2 * t - 2) + 1;
+}
+
 function mapAndClamp(value, low1, high1, low2, high2) {
   return clamp(
 	map(value, low1, high1, low2, high2),
@@ -37,7 +41,7 @@ const fadeLogos = function() {
 				logoDelta = 0 - logoDelta
 			}
 			
-			targetOpacity = mapAndClamp(logoDelta, 0, containerWidth, 1, 0)
+			targetOpacity = mapAndClamp(logoDelta, 0, containerWidth * 0.3, 1, 0)
 			logo.style.opacity = targetOpacity
 		})
 		
@@ -45,20 +49,37 @@ const fadeLogos = function() {
 }
 
 const moveLogos = function() {
-	const yPositionTop = window.pageYOffset
+	let yPositionTop = 0
+	yPositionTop = window.pageYOffset
 	const containerWidth = container.offsetWidth
 	const containerXPos = container.getBoundingClientRect().left
 	
 	const totalHeight = bodyTag.getBoundingClientRect().height - window.innerHeight
+	// const bound = containerWidth - ()
 	
-	movement = mapAndClamp(yPositionTop, 0, totalHeight, 0 - (window.innerWidth), (window.innerWidth / 2))
+	movement = mapAndClamp(yPositionTop, 0, totalHeight, 0 - (containerWidth / 4), (containerWidth / 4))
 	
-	console.log(movement)
 	
 	rows.forEach((logoRow, index) => {
-		offset = (index + 1) * 0.2
-		offset = 0 - offset
-		logoRow.style.transform = `translateX(${movement * offset}px)`
+		// offset = (index + 1) * 0.2
+		// offset = 0 - offset
+		
+		delay = index * 30
+		// const rowWidth = logoRow.getBoundingClientRect().width
+		// const numberOfLogos = logoRow.querySelectorAll("img")
+		
+		// movement = mapAndClamp(yPositionTop, 0, totalHeight, 0 - (rowWidth / 3), (rowWidth / 3))
+		
+		if (index % 1 === 0) {
+			movement = 0 - movement
+		}
+		
+		
+		logoRow.style.transform = `translateX(${movement + delay}px)`
+		
+		// if (index === 1) {
+		// 	console.log(movement)
+		// }
 		
 		// if (index == 0 || index == 3){
 		// 	offset = -0.3
@@ -67,7 +88,7 @@ const moveLogos = function() {
 		// 	offset = -0.4
 		// }
 		
-		logoRow.style.transform = `translateX(${(movement * offset)}px)`
+		// logoRow.style.transform = `translateX(${(movement * offset)}px)`
 	})
 	
 }
